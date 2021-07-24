f290cc463164f19a990f9da0a51ad8858698f778
sahlaysta <51031130+sahlaysta@users.noreply.github.com>
2021-07-24 03:01:17 -0400

    Create CCCEDICTParseException.java

---
+++ CCCEDICTParseException.java
@@ -0,0 +1,8 @@
+package com.github.sahlaysta.cccedict;
+
+public class CCCEDICTParseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	CCCEDICTParseException(String message) {
+		super(message);
+	}
+}
