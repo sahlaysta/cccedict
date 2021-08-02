4c09b65adb1488a4e90d604a7d377dc970bd7476
sahlaysta <51031130+sahlaysta@users.noreply.github.com>
2021-08-01 20:58:04 -0400

    Update CCCEDICTParser.java

--- CCCEDICTParser.java
+++ CCCEDICTParser.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.List;
 
+/** Parser of CC-CEDICT files */
 public class CCCEDICTParser {
 	//Static parse method overloads
 	
