a755ec45bc4a9ad6d3060594437132d9df6d9a82
sahlaysta <51031130+sahlaysta@users.noreply.github.com>
2021-08-01 20:56:25 -0400

    Update CCCEDICTParser.java

--- CCCEDICTParser.java
+++ CCCEDICTParser.java
@@ -7,12 +7,18 @@ import java.util.List;
 
 public class CCCEDICTParser {
 	//Static parse method overloads
+	
+	/** Parses a List of CC-CEDICT Entries from a CC-CEDICT file */
 	public static List<CCCEDICTEntry> parse(InputStreamReader isr) throws IOException {
 		return new CCCEDICTReader(isr).output;
 	}
+	
+	/** Parses a List of CC-CEDICT Entries from a CC-CEDICT file */
 	public static List<CCCEDICTEntry> parse(String filePath) throws IOException {
 		return new CCCEDICTReader(filePath).output;
 	}
+	
+	/** Parses a List of CC-CEDICT Entries from a CC-CEDICT file */
 	public static List<CCCEDICTEntry> parse(File file) throws IOException {
 		return new CCCEDICTReader(file).output;
 	}
