36ebef3f0e57b2f648f5006996b11ea1dae9a5e2
sahlaysta <51031130+sahlaysta@users.noreply.github.com>
2021-08-01 20:56:07 -0400

    Update CCCEDICTParseException.java

--- CCCEDICTParseException.java
+++ CCCEDICTParseException.java
@@ -1,5 +1,6 @@
 package com.github.sahlaysta.cccedict;
 
+/** Parse exception thrown in padding or format error when parsing a CC-CEDICT file */
 public class CCCEDICTParseException extends RuntimeException {
 	private static final long serialVersionUID = 1L;
 	CCCEDICTParseException(String message) {
