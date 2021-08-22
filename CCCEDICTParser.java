b3e284d076aa64be32eb451588b0093721bb2941
sahlaysta <51031130+sahlaysta@users.noreply.github.com>
2021-08-22 00:24:23 -0400

    Update CCCEDICTParser.java

--- CCCEDICTParser.java
+++ CCCEDICTParser.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.nio.charset.StandardCharsets;
@@ -12,9 +13,30 @@ import java.util.List;
 /** Parser of CC-CEDICT files */
 public class CCCEDICTParser {
 
-	/** Parses a List of CC-CEDICT Entries from a Reader. This method does not close the reader */
-	public static List<CCCEDICTEntry> parse(Reader reader) throws IOException {
-		return new CCCEDICTReader(reader).output;
+	/** Parses a List of CC-CEDICT Entries from a Reader */
+	public static List<CCCEDICTEntry> parse(Reader reader, boolean closeReader) throws IOException {
+		List<CCCEDICTEntry> output = new CCCEDICTReader(reader).output;
+		if (closeReader)
+			reader.close();
+		return output;
+	}
+	
+	/** Parses a List of CC-CEDICT Entries from an InputStream */
+	public static List<CCCEDICTEntry> parse(InputStream is, boolean closeInputStream) throws IOException {
+		List<CCCEDICTEntry> output = new CCCEDICTReader(
+				new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8))).output;
+		if (closeInputStream)
+			is.close();
+		return output;
+	}
+	
+	/** Parses a List of CC-CEDICT Entries from an InputStream with the passed buffer size */
+	public static List<CCCEDICTEntry> parse(InputStream is, int bufferSize, boolean closeInputStream) throws IOException {
+		List<CCCEDICTEntry> output = new CCCEDICTReader(
+				new BufferedReader(new InputStreamReader(is, StandardCharsets.UTF_8), bufferSize)).output;
+		if (closeInputStream)
+			is.close();
+		return output;
 	}
 	
 	/** Parses a List of CC-CEDICT Entries from a CC-CEDICT file */
